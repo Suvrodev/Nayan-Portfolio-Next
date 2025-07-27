233a9b0eb426420d55c4fc58238cdd79fb58f3ec
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-07-28 00:21:53 +0600

    Loading page is not for Homepage

--- src/app/(withPublicDashboard)/blog/loading.tsx
+++ src/app/(withPublicDashboard)/blog/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 const LoadingPage = () => {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-br from-[#0f172a] to-[#1e293b] text-white">
+    <div className="min-h-screen w-[100%] flex flex-col items-center justify-center bg-gradient-to-br from-[#0f172a] to-[#1e293b] text-white">
       <Loader2 className="h-12 w-12 animate-spin text-teal-400 mb-4" />
       <p className="text-lg text-white/80 animate-pulse tracking-wide">
         Loading content, please wait...
