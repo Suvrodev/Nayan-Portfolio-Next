85f1ce2700d168dcf42e6576f25f32a0649c31db
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-07-30 05:37:36 +0600

    Added Admin COntact

---
+++ src/app/(withAdminDashboard)/admin-dashboard/admin-contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactPage from "@/app/(withPublicDashboard)/contact/page";
+import React from "react";
+
+const AdminContact = () => {
+  return (
+    <div>
+      <ContactPage />
+    </div>
+  );
+};
+
+export default AdminContact;
