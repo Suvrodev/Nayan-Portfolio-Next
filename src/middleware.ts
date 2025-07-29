9249a5ef2b9f92922a79b507c6dc0852f05a130c
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-07-29 17:24:09 +0600

    Added role based authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,15 +32,40 @@ export const middleware = async (request: NextRequest) => {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  if (user?.role !== "admin") {
-    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  // if (user?.role !== "admin") {
+  //   return NextResponse.redirect(new URL("/unauthorized", request.url));
+  // }
+
+  // Role admin hole admin-dashboard er route e jete parbe
+  if (user.role === "admin") {
+    if (
+      pathname === "/admin-dashboard" ||
+      pathname.startsWith("/admin-dashboard/")
+    ) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
   }
-  // return NextResponse.redirect(new URL("/registration", request.url));
+
+  // Role user hole user-dashboard er route e jete parbe
+  if (user.role === "user") {
+    if (
+      pathname === "/user-dashboard" ||
+      pathname.startsWith("/user-dashboard/")
+    ) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
+  }
+
   return NextResponse.next();
 };
 
 export const config = {
   // matcher: ["/about", "/registration"],
   // matcher: ["/admin-dashboard/:path*"],
-  matcher: ["/admin-dashboard", "/admin-dashboard/:path*"],
+  // matcher: ["/admin-dashboard", "/admin-dashboard/:path*"],
+  matcher: ["/admin-dashboard/:path*", "/user-dashboard/:path*"],
 };
