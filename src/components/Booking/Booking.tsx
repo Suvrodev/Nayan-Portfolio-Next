8d170a667087da700ffa8021cd5d5248bc1a6a16
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-08-15 03:21:44 +0600

    Update booking

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react";
 import { getCalApi } from "@calcom/embed-react";
-import { FaCalendarAlt } from "react-icons/fa"; // Zoom/calendar icon
+// import { FaCalendarAlt } from "react-icons/fa"; // Zoom/calendar icon
 
 const Booking = () => {
   useEffect(() => {
@@ -19,12 +19,22 @@ const Booking = () => {
   }, []);
 
   return (
-    <>
-      {/* Round Booking Button - in case you want custom button */}
-      <div className="fixed bottom-20 right-20">
-        <FaCalendarAlt size={24} />
-      </div>
-    </>
+    // <>
+    //   {/* Round Booking Button - in case you want custom button */}
+    //   <div className="fixed bottom-20 right-20">
+    //     <FaCalendarAlt size={24} />
+    //   </div>
+    // </>
+
+    <div
+      className="fixed bottom-20 right-20 p-4 rounded-full cursor-pointer"
+      onClick={async () => {
+        const cal = await getCalApi({ namespace: "30min" });
+        cal("modal", { calLink: "sarkarnayan/30min" });
+      }}
+    >
+      {/* <FaCalendarAlt size={24} color="white" /> */}
+    </div>
   );
 };
 
