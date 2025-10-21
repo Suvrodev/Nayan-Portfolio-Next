7f686191d0baa0b398afa11e0b97219b57528969
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-10-21 23:23:09 +0600

    update Service detail page padding

--- src/app/(withPublicDashboard)/service/[serviceId]/page.tsx
+++ src/app/(withPublicDashboard)/service/[serviceId]/page.tsx
@@ -71,7 +71,7 @@ const ServiceDetailPage = () => {
   }
 
   return (
-    <div className="max-w-6xl mx-auto p-6 md:p-10">
+    <div className="m-5 md:m-16">
       <ServiceDetailHero service={service} />
 
       {/* For Web Development */}
